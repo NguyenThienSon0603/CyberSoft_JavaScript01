f1db3efd0f46ceb8c9cc045dcb44319d1d3a1c13
NguyenThienSon <nguyenthienson0603@gmail.com>
2024-10-31 00:42:59 +0700

    edit menu

--- View/main.js
+++ View/main.js
@@ -1,30 +1,32 @@
-document.getElementById('btnBaiTap01').onclick = () => {
-    activeMenu('btnBaiTap01');
+const baitap01 = document.getElementById('baitap01');
+const baitap02 = document.getElementById('baitap02');
+const baitap03 = document.getElementById('baitap03');
+const baitap04 = document.getElementById('baitap04');
+const btnBaiTap01 = document.getElementById('btnBaiTap01');
+const btnBaiTap02 = document.getElementById('btnBaiTap02');
+const btnBaiTap03 = document.getElementById('btnBaiTap03');
+const btnBaiTap04 = document.getElementById('btnBaiTap04');
+
+
+btnBaiTap01.onclick = () => {
     document.getElementById('baitap01').classList.toggle('active');
+    removeActive(baitap02, baitap03, baitap04);
 }
-document.getElementById('btnBaiTap02').onclick = () => {
-    activeMenu('btnBaiTap02');
+btnBaiTap02.onclick = () => {
     document.getElementById('baitap02').classList.toggle('active');
-
+    removeActive(baitap01, baitap03, baitap04);
 }
-document.getElementById('btnBaiTap03').onclick = () => {
-    activeMenu('btnBaiTap03');
+btnBaiTap03.onclick = () => {
     document.getElementById('baitap03').classList.toggle('active');
-
+    removeActive(baitap01, baitap02, baitap04);
 }
-document.getElementById('btnBaiTap04').onclick = () => {
-    activeMenu('btnBaiTap04');
+btnBaiTap04.onclick = () => {
     document.getElementById('baitap04').classList.toggle('active');
-
+    removeActive(baitap01, baitap02, baitap03);
 }
 
-function activeMenu(idActive) {
-    let active = document.querySelector('#' + idActive + ' #icon-book');
-
-    if (active.classList.contains('fa-book')) {
-        active.classList.replace('fa-book', 'fa-book-open');
-    }
-    else {
-        active.classList.replace('fa-book-open', 'fa-book');
-    }
+function removeActive(idActive1, idActive2, idActive3){
+    idActive1.classList.remove('active');
+    idActive2.classList.remove('active');
+    idActive3.classList.remove('active');
 }
